d218fe6b96df79616d11eb5e656ca17eee503968
Eric Rochester <erochest@virginia.edu>
2014-07-15 16:18:20 -0400

    Refactored main.

--- IntelligentDesign.hs
+++ IntelligentDesign.hs
@@ -31,19 +31,21 @@ targetFile = "plain.png"
 wrapEl :: Element -> Document
 wrapEl el = Document (Prologue [] Nothing []) el []
 
+procFile :: GenIO -> (FilePath, DynamicImage) -> Int -> IO ()
+procFile g target i = do
+    let i'      = T.justifyRight 5 '0' . T.pack $ show i
+        instDir = outputDir </> fromText i'
+        score   = instDir   </> fromText i' <.> "txt"
+
+    createDirectory True instDir
+    el <- generateElement 0.4 10 g
+    fitness <- matchScreen target instDir (Gene i' Spontaneous el)
+    TIO.writeFile (encodeString score) . T.pack $ show fitness
+
 
 main :: IO ()
 main = withSystemRandom $ \g -> do
     target' <- fmap ((,) targetFile) <$> readImage (encodeString targetFile)
     case target' of
-        Right target -> do
-            forM_ ([1..1000] :: [Int]) $ \i -> do
-                let i'      = T.justifyRight 5 '0' . T.pack $ show i
-                    instDir = outputDir </> fromText i'
-                    score   = instDir   </> fromText i' <.> "txt"
-
-                createDirectory True instDir
-                el <- generateElement 0.4 10 g
-                fitness <- matchScreen target instDir (Gene i' Spontaneous el)
-                TIO.writeFile (encodeString score) . T.pack $ show fitness
-        Left err -> error $ "Unable to read image: " ++ err
+        Right target -> forM_ [1..1000] $ procFile g target
+        Left err     -> error $ "Unable to read image: " ++ err
