a00072291cfbf096ddca25f0cbad908dd15e931c
Eric Rochester <erochest@gmail.com>
2022-02-26 12:16:16 -0500

    Added Program type

--- id-lang/src/lib.rs
+++ id-lang/src/lib.rs
@@ -6,8 +6,6 @@ mod error;
 
 use crate::error::{Error, Result};
 
-// TODO: Add Program type
-
 #[derive(Debug, PartialEq)]
 pub enum Token {
     IntLiteral(isize),
@@ -28,6 +26,8 @@ impl FromStr for Token {
         }
     }
 }
+
+type Program = Vec<Token>;
 
 pub fn parse_id<R: Read>(input: &mut R) -> Result<Vec<Result<Token>>> {
     let mut buffer = String::new();
